1564643f5a715ea3b64c887bf42889050793096a
Kiefer WIESSLER <kwiessle@e2r9p11.42.fr>
2017-10-10 18:09:09 +0200

    React & Redux

--- src/client/app.js
+++ src/client/app.js
@@ -3,20 +3,89 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MainCSS from './stylesheets/main.scss';
 import io from 'socket.io-client';
+// import Redux from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 const Socket = io.connect(window.location.host, {reconnect: true});
+// const Redux = require('redux');
 
 Socket.emit('connected', {message: '[SOCKETS] -- socket.io Connected'});
 
+const initialState = [
+    {
+      id: 0,
+      name: 'Product 1',
+      quantity: 0
+    },
+    {
+      id: 1,
+      name: 'Product 2',
+      quantity: 0
+    }
+];
 
-
-class Hello extends React.Component {
-  render () {
-    return (
-      <div className="hud">
-        React Main Component here.
-      </div>
-    );
-  }
+const productState = (state = initialState, action) => {
+    console.log(action);
+    if (action.type == 'ADD_TO_CART') {
+        state[action.index].quantity += 1
+        return (state);
+    }
+    return (state);
 }
 
-ReactDOM.render(<Hello />, document.getElementById('main'));
+const store = createStore(
+    combineReducers({
+      productState
+  })
+)
+
+
+const Product = (props) => {
+    return (
+      <div>
+      <h2>{props.name}</h2>
+      <p>Quantity: {props.quantity}</p>
+      <button onClick={() => store.dispatch({
+          type: 'ADD_TO_CART',
+          index: props.id
+      })}>Click Me</button>
+      </div>
+    )
+
+}
+
+const Header = () => {
+  return (
+    <div>Product(s) in Cart :
+    {store.getState().productState.reduce((prodA, prodB) => (
+      prodA.quantity + prodB.quantity
+    ))}
+    </div>
+  )
+}
+
+const App = () => {
+    return (
+      <div>
+        <Header />
+        {
+          store.getState().productState.map((product) => {
+            return (
+              <Product
+                name={product.name}
+                quantity={product.quantity}
+                id={product.id}
+              />
+            )
+          })
+        }
+      </div>
+    )
+}
+
+const Render = () => {
+    ReactDOM.render(<App />, document.getElementById('main'));
+}
+Render();
+store.subscribe(() => {
+  Render();
+})
